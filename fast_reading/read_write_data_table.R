59fc1e9b95521e0daefe10f9751168f3213f70af
Tam Nguyen <tamnva@gmail.com>
2024-09-13 21:39:54 +0200

    Update read_write_data_table.R

--- fast_reading/read_write_data_table.R
+++ fast_reading/read_write_data_table.R
@@ -2,15 +2,16 @@
 # Install/Load required package-------------------------------------------------
 install.packages("data.table")
 
-# Load readr package
+# Load data.table package
 library(data.table)
 
 # Download and read data--------------------------------------------------------
-# https://github.com/tamnva/r_course_material/tree/master/example_data_readr
+# Link in this video description
+# https://github.com/tamnva/r_course_material/archive/refs/heads/master.zip
 
 # Read us_economic_data.csv
 # Note: first column should be read as date
-data <- fread(file = "C:/examples/us_economic_data.csv",
+data <- fread(file = "C:/fast_reading/us_economic_data.csv",
               sep = ",",
               dec = ".",
               header = TRUE)
@@ -18,23 +19,24 @@ data <- fread(file = "C:/examples/us_economic_data.csv",
 
 # Read climate_data_berlin_dwd.txt
 # The STATIONS_ID and MESS_DATUM should be read as character
-data <- fread(file = "C:/examples/climate_data_berlin_dwd.txt", 
+data <- fread(file = "C:/fast_reading/climate_data_berlin_dwd.txt", 
               sep = ";",
               header = TRUE, 
               colClasses = setNames(c("character", "character"),
                                     c("STATIONS_ID", "MESS_DATUM")))
-
+      
 data$MESS_DATUM <- as.Date(data$MESS_DATUM, format = "%Y%m%d")
 
 # Read output.rch 
 # Note: no header, need to skip first 9 lines
-data <- fread(file = "C:/examples/output.rch", dec=".",
+data <- fread(file = "C:/fast_reading/output.rch", 
+              dec=".",
               header = FALSE, 
               skip = 9)
 
 # Write data--------------------------------------------------------------------
 # Write data to text file
-fwrite(data, file = "C:/examples/data.txt")
+fwrite(data, file = "C:/fast_reading/data.txt")
 
 
 
