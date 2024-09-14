af9158ed12e5f79496604d6acccfc9aa0ec05f8f
Tam Nguyen <tamnva@gmail.com>
2024-09-14 13:15:53 +0200

    update

--- visualization/visualization.R
+++ visualization/visualization.R
@@ -1,15 +1,30 @@
 
-# General syntax ---------------------------------------------------------------
-#ggplot (data = <DATA> ) +
-#  <GEOM_FUNCTION> (mapping = aes( <MAPPINGS> ),
-#                   stat = <STAT> , position = <POSITION> ) +
-#  <COORDINATE_FUNCTION> +
-#  <FACET_FUNCTION> +
-#  <SCALE_FUNCTION> +
-#  <THEME_FUNCTION>
+# Links to this R script + data (climate_berlin.txt) are in the video description
 
-# Plot climate data ------------------------------------------------------------
-ggplot() + 
-  geom_line(aes(x = xval, y = yval)) +
-  geom_point(aes(x = xval, y = yval))
+# Load ggplot2 library
+library(ggplot2)
+
+#ggplot (data = <DATA> ) +                                     # required
+#  <GEOM_FUNCTION> (mapping = aes( <MAPPINGS> ),               # required
+#                   stat = <STAT> , position = <POSITION> ) +  # -optional
+#  <COORDINATE_FUNCTION> +                                     # -optional
+#  <FACET_FUNCTION> +                                          # -optional
+#  <SCALE_FUNCTION> +                                          # -optional
+#  <LABEL, THEME_FUNCTION,...>                                 # -optional
+
+# Read data --------------------------------------------------------------------
+data <- read.csv("C:/examples/climate_berlin.txt", header = TRUE, sep = ",")
+data$date <- as.Date(data$date, format("%Y-%m-%d"))
+data$year <- format(data$date, "%Y")
+
+# plot with ggplot -------------------------------------------------------------
+ggplot(data = data) + 
+  geom_line(aes(x = date, y = t_ave, color = t_ave), linewidth = 0.5) +
+  coord_fixed(ratio = 10) +
+  facet_grid(row = vars(year)) +
+  scale_colour_gradientn(name = "temperature", colours = c("blue",  "orange")) +
+  labs(x = " ", y = "temperature") +
+  theme(legend.position = "top")
+
+ggsave(filename = "C:/examples/myplot.pdf", width = 6, height = 4)
 
