5659ae44116bb0f0ed95e0533adf958b71dea287
Tam Nguyen <tamnva@gmail.com>
2024-10-04 21:55:37 +0200

    play game with R

---
+++ playing_game_with_r/gaming_with_r.R
@@ -0,0 +1,12 @@
+# Install Rcade package
+devtools::install_github('RLesur/Rcade')
+
+# Load Rcade package
+library(Rcade)
+
+# List all games
+games
+
+# Play a specific game
+games$Pacman
+games$CathTheEgg
