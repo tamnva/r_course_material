417544f57599a7c5abfdb90df4f8bc36313ed4e5
Tam Nguyen <tamnva@gmail.com>
2024-10-04 20:35:04 +0200

    Update save_big_data.R

--- save_big_data/save_big_data.R
+++ save_big_data/save_big_data.R
@@ -3,7 +3,7 @@
 setwd("C:/examples")
 
 # Create example data (10 million rows x 10 columns)
-large_data <- data.frame(1:10^8, ncol = 10) + 0.123456789
+large_data <- matrix(runif(10^8), ncol = 10)
 
 # Save as .csv files
 write.csv(large_data, "large_data.csv", row.names = FALSE)
@@ -13,14 +13,13 @@ saveRDS(large_data, "large_data.rds")
 
 # Save as parquet
 library(arrow)
-write_parquet(large_data, "large_data.parquet")
+write_parquet(as.data.frame(large_data), "large_data.parquet")
 
 # Save as netCDF
 library(ncdf4)
-data_array <- as.matrix(large_data)
 
-dimX <- ncdim_def(name = "row", units = "", vals = 1:nrow(data_array))
-dimY <- ncdim_def(name = "column", units = "", vals = 1:ncol(data_array))
+dimX <- ncdim_def(name = "row", units = "", vals = 1:nrow(large_data))
+dimY <- ncdim_def(name = "column", units = "", vals = 1:ncol(large_data))
 
 var_def <- ncvar_def(name = "data", units = "", 
                      dim = list(dimX, dimY), 
@@ -28,6 +27,8 @@ var_def <- ncvar_def(name = "data", units = "",
                      longname = "Data from data frame")
 
 ncfile <- nc_create("large_data.nc", var_def) # Create NetCDF file
-ncvar_put(ncfile, var_def, data_array)    # Write data to file
+ncvar_put(ncfile, var_def, large_data)    # Write data to file
 nc_close(ncfile)                          # Close the NetCDF file
 
+# Save as feather
+write_feather(as.data.frame(large_data), "large_data.feather")
